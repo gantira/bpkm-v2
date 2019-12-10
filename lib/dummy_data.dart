bc8e04634f7ec7f2d12933e4b538a5a35cefd600
Angga Gantira <angga.gantira@gmail.com>
2019-12-10 14:07:45 +0700

    Update Link

--- lib/dummy_data.dart
+++ lib/dummy_data.dart
@@ -32,11 +32,6 @@ const CATEGORIES_DUMMY_DATA = const [
     title: 'Kemitraan',
     description: '',
   ),
-  Category(
-    id: 'W7',
-    title: 'KA UNIKOM',
-    description: '',
-  ),
 ];
 
 const WEBSITE_DUMMY_DATA = const [
@@ -45,14 +40,14 @@ const WEBSITE_DUMMY_DATA = const [
     name: 'Statistik NIB',
     description: 'Tanjung bira terletak di daerah Sulawesi Selatan. Lokasi khususnya adalah Kabupaten Bulukumba yang jaraknya sangat jauh, sekitar 200 km dari pusat ibukota Makassar. Sehingga tanjung bira bisa diaktakan terletak di ujung selatan daratan sulawesi selatan. Tanjung bira memang bisa dikatakan senjata oleh masyarakat sekitar untuk menarik masyarakat luar dan wisatawan baik lokal maupun asing untuk datang ke sana. Tanjung bira tentu saja menawarkan hal seperti pantai putih, alam bawah laut dan juga pemandangan senja yang tidak ada duanya.',
     category: 'W1',
-    url: "http://35.198.244.49:3000/public/dashboard/7d7f45d2-deeb-4d90-9579-597fdada1ab1",
+    url: "http://35.198.244.49:3001/public/dashboard/5d1bea02-584b-4a97-8953-3087d946fc1b",
   ),
   Website(
     id: 'A2',
     name: 'Top Perusahaan',
     description: 'Soal indah, Pantai Bara dan Pantai Bira ibarat saudara kembar. Selain namanya yang mirip, letaknya sama-sama di Bulukumba, Sulsel. Pantai pasir putih nan lembut dan laut biru ada di sini. Pantai Bara memang belum setenar Tanjung Bira. Meski demikian, pemandangan di sini tak kalah menarik. Hamparan pasir putih dipadukan dengan tenangnya laut dari arah Flores, membuat kita betah berlama-lama di Pantai Bara.',
     category: 'W1',
-    url: "http://35.198.244.49:3000/public/dashboard/54b2d08b-98cb-4071-b274-fd45285f7ef4",
+    url: "#",
   ),
 
   Website(
@@ -60,7 +55,7 @@ const WEBSITE_DUMMY_DATA = const [
     name: 'Izin Usaha',
     description: 'Di sini kamu dapat menemukan banyak rumah tongkonan yang telah berdiri semenjak zaman leluhur. Di atas bagian pintu masuk kamu dapat menemukan kepala kerbau yang ditempelkan di dinding. Dalam budaya masyarakat Toraja, hal tersebut memiliki arti kemakmuran, kejayaan, dan status sosial.',
     category: 'W2',
-    url: "http://35.198.244.49:3000/public/dashboard/2c368f9a-318d-4766-b252-9a8e1a33caf7",
+    url: "http://35.198.244.49:3001/public/dashboard/367010de-534c-4ca3-9ef3-eb5159def7ff",
   ),
 
   Website(
@@ -68,7 +63,7 @@ const WEBSITE_DUMMY_DATA = const [
     name: 'Izin Komersial',
     description: 'Suku Kajang Ammatoa terletak di kabupaten Bulukumba, Kecamatan Kajang, Sulawesi Selatan. Desa ini dinamakan Tana Toa yang merupakan tanah yang tertua di dunia dikarenakan kepercayaan masyarakat adatnya. Secara geografis, luas wilayah Desa Kajang Ammatoa sekitar 331,17 ha dan memiliki kondisi hutan yang sangat lebat. Hampir seluruh dusun yang berada di dalamnya di kelilingi hutan dan tidak ada jalan beraspal di dalam kawasan ini.',
     category: 'W2',
-    url: "http://35.198.244.49:3000/public/dashboard/1f3c4162-bceb-4dd7-a5d9-359112a800d5",
+    url: "#",
   ),
 
   Website(
@@ -76,14 +71,14 @@ const WEBSITE_DUMMY_DATA = const [
     name: 'Sektor & Lokasi Proyek',
     description: 'Tanjung bira terletak di daerah Sulawesi Selatan. Lokasi khususnya adalah Kabupaten Bulukumba yang jaraknya sangat jauh, sekitar 200 km dari pusat ibukota Makassar. Sehingga tanjung bira bisa diaktakan terletak di ujung selatan daratan sulawesi selatan. Tanjung bira memang bisa dikatakan senjata oleh masyarakat sekitar untuk menarik masyarakat luar dan wisatawan baik lokal maupun asing untuk datang ke sana. Tanjung bira tentu saja menawarkan hal seperti pantai putih, alam bawah laut dan juga pemandangan senja yang tidak ada duanya.',
     category: 'W3',
-    url: "http://35.198.244.49:3000/public/dashboard/141adc75-46db-49f3-8856-7cb37dd03cad",
+    url: "http://35.198.244.49:3001/public/dashboard/78e70bf3-1fc4-404a-bec5-315b92a6547b",
   ),
   Website(
     id: 'A6',
     name: 'Negara & Kementerian',
     description: 'Soal indah, Pantai Bara dan Pantai Bira ibarat saudara kembar. Selain namanya yang mirip, letaknya sama-sama di Bulukumba, Sulsel. Pantai pasir putih nan lembut dan laut biru ada di sini. Pantai Bara memang belum setenar Tanjung Bira. Meski demikian, pemandangan di sini tak kalah menarik. Hamparan pasir putih dipadukan dengan tenangnya laut dari arah Flores, membuat kita betah berlama-lama di Pantai Bara.',
     category: 'W3',
-    url: "",
+    url: "#",
   ),
 
   Website(
@@ -91,7 +86,7 @@ const WEBSITE_DUMMY_DATA = const [
     name: 'Daerah & Kawasan ',
     description: 'Di sini kamu dapat menemukan banyak rumah tongkonan yang telah berdiri semenjak zaman leluhur. Di atas bagian pintu masuk kamu dapat menemukan kepala kerbau yang ditempelkan di dinding. Dalam budaya masyarakat Toraja, hal tersebut memiliki arti kemakmuran, kejayaan, dan status sosial.',
     category: 'W4',
-    url: "http://35.198.244.49:3000/public/dashboard/dfd978b1-3c8d-42e0-8c8f-dc3f8246ca93",
+    url: "http://35.198.244.49:3001/public/dashboard/fa560d6a-d638-4be9-a509-3e8b022ba3d9",
   ),
 
   Website(
@@ -99,7 +94,7 @@ const WEBSITE_DUMMY_DATA = const [
     name: 'Bandara & Pelabuhan',
     description: 'Suku Kajang Ammatoa terletak di kabupaten Bulukumba, Kecamatan Kajang, Sulawesi Selatan. Desa ini dinamakan Tana Toa yang merupakan tanah yang tertua di dunia dikarenakan kepercayaan masyarakat adatnya. Secara geografis, luas wilayah Desa Kajang Ammatoa sekitar 331,17 ha dan memiliki kondisi hutan yang sangat lebat. Hampir seluruh dusun yang berada di dalamnya di kelilingi hutan dan tidak ada jalan beraspal di dalam kawasan ini.',
     category: 'W4',
-    url: "",
+    url: "#",
   ),
 
   Website(
@@ -107,14 +102,14 @@ const WEBSITE_DUMMY_DATA = const [
     name: 'Realisasi Investasi Per Sektor',
     description: 'Tanjung bira terletak di daerah Sulawesi Selatan. Lokasi khususnya adalah Kabupaten Bulukumba yang jaraknya sangat jauh, sekitar 200 km dari pusat ibukota Makassar. Sehingga tanjung bira bisa diaktakan terletak di ujung selatan daratan sulawesi selatan. Tanjung bira memang bisa dikatakan senjata oleh masyarakat sekitar untuk menarik masyarakat luar dan wisatawan baik lokal maupun asing untuk datang ke sana. Tanjung bira tentu saja menawarkan hal seperti pantai putih, alam bawah laut dan juga pemandangan senja yang tidak ada duanya.',
     category: 'W5',
-    url: "http://35.198.244.49:3000/public/dashboard/a80b8398-603b-4a40-84a1-23a58d42bff2",
+    url: "http://35.198.244.49:3001/public/dashboard/7caa834d-b294-4203-8dbd-8ade52d50655",
   ),
   Website(
     id: 'A10',
     name: 'Realisasi Investasi Per Lokasi',
     description: 'Soal indah, Pantai Bara dan Pantai Bira ibarat saudara kembar. Selain namanya yang mirip, letaknya sama-sama di Bulukumba, Sulsel. Pantai pasir putih nan lembut dan laut biru ada di sini. Pantai Bara memang belum setenar Tanjung Bira. Meski demikian, pemandangan di sini tak kalah menarik. Hamparan pasir putih dipadukan dengan tenangnya laut dari arah Flores, membuat kita betah berlama-lama di Pantai Bara.',
     category: 'W5',
-    url: "http://35.198.244.49:3000/public/dashboard/ba7acaa9-3441-4a32-89d7-edaa97c1577c",
+    url: "#",
   ),
 
   Website(
@@ -122,7 +117,7 @@ const WEBSITE_DUMMY_DATA = const [
     name: 'Perusahaan',
     description: 'Di sini kamu dapat menemukan banyak rumah tongkonan yang telah berdiri semenjak zaman leluhur. Di atas bagian pintu masuk kamu dapat menemukan kepala kerbau yang ditempelkan di dinding. Dalam budaya masyarakat Toraja, hal tersebut memiliki arti kemakmuran, kejayaan, dan status sosial.',
     category: 'W6',
-    url: "http://35.198.244.49:3000/public/dashboard/c8a4ef03-2fff-4b75-aeb9-7242095e0438",
+    url: "http://35.198.244.49:3001/public/dashboard/a44752f5-ff97-43c3-a88c-ab21041e99c1",
   ),
 
   Website(
@@ -131,12 +126,5 @@ const WEBSITE_DUMMY_DATA = const [
     description: 'Suku Kajang Ammatoa terletak di kabupaten Bulukumba, Kecamatan Kajang, Sulawesi Selatan. Desa ini dinamakan Tana Toa yang merupakan tanah yang tertua di dunia dikarenakan kepercayaan masyarakat adatnya. Secara geografis, luas wilayah Desa Kajang Ammatoa sekitar 331,17 ha dan memiliki kondisi hutan yang sangat lebat. Hampir seluruh dusun yang berada di dalamnya di kelilingi hutan dan tidak ada jalan beraspal di dalam kawasan ini.',
     category: 'W6',
     url: "",
-  ),
-  Website(
-    id: 'A13',
-    name: 'KA UNIKOM',
-    description: 'Suku Kajang Ammatoa terletak di kabupaten Bulukumba, Kecamatan Kajang, Sulawesi Selatan. Desa ini dinamakan Tana Toa yang merupakan tanah yang tertua di dunia dikarenakan kepercayaan masyarakat adatnya. Secara geografis, luas wilayah Desa Kajang Ammatoa sekitar 331,17 ha dan memiliki kondisi hutan yang sangat lebat. Hampir seluruh dusun yang berada di dalamnya di kelilingi hutan dan tidak ada jalan beraspal di dalam kawasan ini.',
-    category: 'W7',
-    url: "https://ka.unikom.ac.id/",
   ),
 ];
